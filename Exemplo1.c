92567b33d7cf7ee354110562f0d9e1a9998fcacc
RmGarden <raquelfmartins.0@gmail.com>
2025-07-30 16:25:45 +0100

    test

--- Exemplo1.c
+++ Exemplo1.c
@@ -6,7 +6,7 @@
 int main ()
 {
     // Variable types
-    int a=12;
+    int a=12, x=5, y;
     float b=4.5;
     char c='s';
 
@@ -18,6 +18,13 @@ int main ()
 
     //Concatenation
     printf("\n\nO valor de A é %d. \nO valor de B é %.2f. \nO valor de C é %c.", a,b,c);
+
+    //Incrementos
+    y=x++; // Primeiro atribui e depois incrementa y=5 x=6
+    printf("\n\nValor x= %d e o valor de y = %d\n",x,y);
+    
+    y=++x; // Primeiro incrementa e depois atribui x=7 y=7
+    printf("valor x = %d e o valor de y = %d",x,y);
 
      
     return 0;
